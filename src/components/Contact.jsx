1cdd65b66cf2c4f6f21e727e39b4ab9addc8619f
Steve Ezeala <120297848+Scode75@users.noreply.github.com>
2023-08-17 22:54:09 +0100

    Updated Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action='https://getform.io/f/4a491a63-6492-4c10-9bfd-3137cd595779' className='flex flex-col max-w-[600px] w-full'>
           <div className='pb-8'>
-            <p className='text-4xl font-bold inlin border-b-4 border-red-600 text-gray-300'>Conatct</p>
+            <p className='text-4xl font-bold inlin border-b-4 border-red-600 text-gray-300'>Contact</p>
             <p className='text-gray-300 py-4'>// Submit the form below or send me an email - stevanth75@gmail.com</p>
           </div>
           <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
