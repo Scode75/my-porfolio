d85549f6ebfb78b5cf73f843e4b61b39eab8f15a
Steve Ezeala <120297848+Scode75@users.noreply.github.com>
2023-08-17 23:53:19 +0100

    Updated my desc Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Steve Ezeala</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front-End Developer</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a frontend developer specializing in building (and occasionally designing) exceptional digital experiences. 
-                Currently, I'm focused on building responsive full-stack web applications.</p>
+                Currently, I'm focused on building responsive web applications.</p>
             <div>
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>View Work 
                 <span className='group-hover:rotate-90 duration-300'>
